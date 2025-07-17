b734e4847be3c0a734691ad79293342fa98a3ba9
Mohammad Ashrafi <ashrafi.dev221184@gmail.com>
2025-07-17 22:31:19 +0430

    -

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-// import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+const redirect = sessionStorage.redirect;
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+  window.history.replaceState(null, "", redirect);
+}
+
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <StrictMode>
-        <App />
-      </StrictMode>
-    </BrowserRouter>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
 )
